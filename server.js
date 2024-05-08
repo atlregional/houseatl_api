e5cbfc46d5951d9748893bb7ce7122a0af920be8
ASMBLG <erik.woodworth@asmblg.io>
2024-05-08 12:58:27 -0500

    cors reconfigured

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express'),
 	morgan = require('morgan'),
 	compression = require('compression'),
+  cors = require('cors'),
 	mongoose = require('mongoose');
 
 const app = express();
@@ -14,15 +15,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(compression());
+app.use(cors());
 
 // https://house-atl-api-dev.herokuapp.com/
 
-app.use((req, res, next) => {
-	res.append('Access-Control-Allow-Origin', ['*']);
-	res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-	res.append('Access-Control-Allow-Headers', 'Content-Type');
-	next();
-});
+// app.use((req, res, next) => {
+// 	res.append('Access-Control-Allow-Origin', ['*']);
+// 	res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+// 	res.append('Access-Control-Allow-Headers', 'Content-Type');
+// 	next();
+// });
 
 app.use(routes);
 
