5d18ad7dbf0163d71d13f5c2c0c19e265d330eca
ASMBLG <erik.woodworth@asmblg.io>
2024-05-28 16:03:38 -0500

    added get route for geos and data

--- routes/data/index.js
+++ routes/data/index.js
@@ -3,6 +3,8 @@ const dataController = require('../../controllers/dataController');
 
 router.route('/').get(dataController.findAll);
 router.route('/optimized').post(dataController.find);
+router.route('/optimized').get(dataController.find);
+
 
 
 module.exports = router;
--- routes/geos/index.js
+++ routes/geos/index.js
@@ -3,6 +3,7 @@ const geoController = require('../../controllers/geoController.js');
 
 // router.route('/').get(dataController.findAll);
 router.route('/').post(geoController.find);
+router.route('/').get(geoController.find);
 
 
 module.exports = router;
